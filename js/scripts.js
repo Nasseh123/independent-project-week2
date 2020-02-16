c094dffc840d644fcf1fc79041cff3e46ef1920f
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-02-16 11:50:35 +0300

    addition of inner.html to output the result to the index.html> ;

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,17 @@ var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",];
 //calling functions for the Inputs
 function getName(){
     var year=parseInt(document.getElementById("year").value);
-    alert (year/4)
+    //alert (year/4)
     var month=parseInt(document.getElementById("month").value);
     var day=parseInt(document.getElementById("day").value);
     var male=document.getElementById("male");
     var female=document.getElementById("female");
-    alert(true)
     var vali=((day>0 || day<=31)&&(month>0||month<=12 ));
     var vali2=(((year%4)==0 && month==2 && day<=28 ));
     var vali3=((year%4)!==0 &&month==2  && day<=29);
-    alert (vali)
-    alert(vali2)
-    alert(vali3)
+    //alert (vali)
+    //alert(vali2+ "valll222")
+    //alert(vali3 +"val33")
     
 
     if (day<=0 || day>31 ){
@@ -31,13 +30,14 @@ function getName(){
     }else if ((year%4)!==0 &&month==2  && day>29){
         alert ( "Input correct date!\n \n "+ year +" "+ "Has 29 days on February")
     }
-    alert("validation complete")
+    //alert("validation complete")
     var day =new Date (year + "/" + month + "/"+ day);
-    alert (day);
+    //alert (day);
     var birthDay=day.getDay();
-    alert (birthDay+ "the dayyy")
-    if (vali== true && vali2 == true&& vali3==true&&male.checked==true){
-        alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleNames[birthDay]);
+    //alert (birthDay+ "the dayyy")
+    if (vali== true && male.checked==true){
+       // alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleNames[birthDay]);
+        document.getElementById("demo").innerHTML = "You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleNames[birthDay];
     }else if (female.checked){
         alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + femaleNames[birthDay])
 
